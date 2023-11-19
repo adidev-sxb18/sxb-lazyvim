e633765810903765ffc097c9ec7dc503dd8b15c7
adidev-sxb18 <adidev.sudoxbyte@gmail.com>
2023-11-19 18:06:55 +0530

    sxb_v1

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local Util = require("lazyvim.util")
+
+local map = Util.safe_keymap_set
+map("n", "<leader>lu", "<cmd>Lazy update<cr>", { desc = "Plugs-Update" })
+map("n", "<leader>rr", "<cmd>RunCode<cr>", { desc = "RunCode" })
