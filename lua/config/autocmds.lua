3d84b33765dc00b92a4640e0ff90ff7905ab6ebc
adidev-sxb18 <adidev.sudoxbyte@gmail.com>
2023-11-21 21:34:51 +0530

    learning-autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+    vim.keymap.set("n", "<leader>rl", "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", opts)
+  end,
+})
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,4 @@ local Util = require("lazyvim.util")
 local map = Util.safe_keymap_set
 map("n", "<leader>lu", "<cmd>Lazy update<cr>", { desc = "Plugs-Update" })
 map("n", "<leader>rr", "<cmd>RunCode<cr>", { desc = "RunCode" })
+map("n", "<leader>la", "TSUpdateSync", { desc = "Treesitter Sync" })
