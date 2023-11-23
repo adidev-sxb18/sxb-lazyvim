6a82d227d50b8c66341c1022479a7bd28befdc7b
adidev-sxb18 <adidev.sudoxbyte@gmail.com>
2023-11-23 22:38:12 +0530

    plugs-upd-2311.1

--- lua/plugins/start.lua
+++ lua/plugins/start.lua
@@ -3,7 +3,7 @@ return {
   event = "VimEnter",
   opts = function(_, opts)
     local logo = [[
-          "ShivByte.rust"
+          "ShivByte.rs"
   ]]
     logo = string.rep("\n", 8) .. logo .. "\n\n"
     opts.config.header = vim.split(logo, "\n")
