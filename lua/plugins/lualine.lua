8dba991772c4a0397681e204eeefc1b005d95491
adidev-sxb18 <adidev.sudoxbyte@gmail.com>
2023-11-29 22:27:14 +0530

    29-11.0

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,14 +9,14 @@ return {
       },
       sections = {
         lualine_a = {
-          { "mode", separator = { left = "" }, right_padding = 2 },
+          { "mode", separator = { left = "" }, right_padding = 3 },
         },
         lualine_b = { "filename", "branch" },
         lualine_c = { "fileformat" },
         lualine_x = {},
         lualine_y = { "filetype", "progress" },
         lualine_z = {
-          { "location", separator = { right = "" }, left_padding = 2 },
+          { "location", separator = { right = "" }, left_padding = 3 },
         },
       },
       inactive_sections = {
