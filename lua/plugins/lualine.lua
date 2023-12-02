33fb92f7b3c21cb01e1580e54a8a03ac7115bbf1
adidev-sxb18 <adidev.sudoxbyte@gmail.com>
2023-12-02 19:25:54 +0530

    12-2.0

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -13,7 +13,7 @@ return {
         },
         lualine_b = { "filename", "branch" },
         lualine_c = { "fileformat" },
-        lualine_x = {},
+        lualine_x = { "lsp_progress" },
         lualine_y = { "filetype", "progress" },
         lualine_z = {
           { "location", separator = { right = "" }, left_padding = 3 },
--- lua/plugins/plugs.lua
+++ lua/plugins/plugs.lua
@@ -2,4 +2,6 @@ return {
   "srcery-colors/srcery-vim",
   "olimorris/onedarkpro.nvim",
   "Mofiqul/vscode.nvim",
+  "LunarVim/darkplus.nvim",
+  "arkav/lualine-lsp-progress",
 }
