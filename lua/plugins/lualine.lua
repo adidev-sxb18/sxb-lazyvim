5021709b05942ae5e47ab5f925fa46f845b34c17
adidev-sxb18 <adidev.sudoxbyte@gmail.com>
2023-12-25 16:34:48 +0530

    2512.01-nuf

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,19 +4,23 @@ return {
     require("lualine").setup({
       options = {
         theme = onedark_dark,
+        icons_enabled = true,
         component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
       },
+      always_divide_middle = true,
       sections = {
         lualine_a = {
-          { "filename", "branch", separator = { left = "" }, right_padding = 3 },
+          { "filename", "branch", separator = { left = "", right = "" }, right_padding = 3 },
         },
         lualine_b = { "filetype" },
-        lualine_c = { "fileformat" },
+        lualine_c = {
+          "diagnostics",
+        },
         lualine_x = { "lsp_progress" },
         lualine_y = { "mode" },
         lualine_z = {
-          { "datetime", separator = { right = "" }, left_padding = 3 },
+          { "datetime", separator = { left = "", right = "" }, left_padding = 3 },
         },
       },
       inactive_sections = {
